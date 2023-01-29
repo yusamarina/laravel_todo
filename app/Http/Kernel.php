7f52fca398e0dfeca3164fe810e3f68029378ddc
yusamarina <94360716+yusamarina@users.noreply.github.com>
2023-01-29 17:51:24 +0900

    Add:その他設定を追加

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\TaskMiddleware::class,
     ];
 
     /**
@@ -45,6 +46,10 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+
+        'task' => [
+            \App\Http\Middleware\TaskMiddleware::class,
+        ],
     ];
 
     /**
@@ -65,5 +70,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'task' => \App\Http\Middleware\TaskMiddleware::class,
     ];
 }
