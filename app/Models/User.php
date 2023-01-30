a625124aff7d8733407673298b01210fd0f34cf3
yusamarina <94360716+yusamarina@users.noreply.github.com>
2023-01-30 16:26:25 +0900

    Add:Breezeで認証機能を作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,8 +42,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function posts()
+    // public function posts()
+    // {
+    //     return $this->hasMany('App\Models\Task');
+    // }
+
+    public function tasks()
     {
-        return $this->hasMany('App\Models\Task');
+        return $this->hasMany('App\Tasks');
     }
 }
