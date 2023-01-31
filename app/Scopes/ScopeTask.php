0dd707faa0c9ac85665224ac50461582a4327379
yusamarina <94360716+yusamarina@users.noreply.github.com>
2023-01-31 21:27:14 +0900

    Fix:スコープの修正

--- app/Scopes/ScopeTask.php
+++ app/Scopes/ScopeTask.php
@@ -5,11 +5,13 @@ namespace App\Scopes;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class ScopeTask implements Scope
 {
   public function apply(Builder $builder, Model $model)
   {
-    $builder->where('status', '=', 0);
+    // $builder->where('status', '=', 0);
+    $builder->where('user_id', '=', Auth::id());
   }
 }
