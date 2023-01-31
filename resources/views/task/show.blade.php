33c795f3f4e852b57f9dbf6582ae01e1f649542d
yusamarina <94360716+yusamarina@users.noreply.github.com>
2023-01-31 22:48:17 +0900

    Fix:UIの修正

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -5,21 +5,27 @@
       </h2>
   </x-slot>
 
-  <div class="max-w-sm w-full lg:max-w-full lg:flex justify-center text-2xl py-6">
-    <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
-      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{ $item->title }}</h3>
-        <p class="text-xl my-4 font-light">Memo: {{ $item->memo }}</p>
-      <figcaption class="flex items-center justify-center space-x-3">
-        <div class="text-xl space-y-0.5 font-medium dark:text-white text-left">
-          @if (old('status', $item->status ? '1' : '0') === "1")
-            <label for="status">Status: 完了</label>
-          @else
-            <label for="status" class="text-red-600">Status: 未着手</label>
-          @endif
-          <div class="text-sm font-light text-gray-500 dark:text-gray-400 pb-3">{{ $item->created_at }}</div>
-          <a href="{{ route('task_index') }}" class="m-1 bg-yellow-100 hover:bg-yellow-200 text-blue-900 py-2 px-10 border border-blue-900 rounded-full shadow">ToDo Listへ</a>
-        </div>
-      </figcaption>
-    </figure>
-  </div>
+  <table>
+    {{-- <div class="mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2"> --}}
+    <div class="max-w-sm w-full lg:max-w-full lg:flex justify-center text-2xl py-6">
+      <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+        <div class="text-sm font-light text-gray-500 dark:text-gray-400 pb-3">{{ $item->created_at }}</div>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{ $item->title }}</h3>
+          <p class="text-xl my-4 font-light">{{ $item->memo }}</p>
+        <figcaption class="flex items-center justify-center space-x-3">
+          <div class="text-xl space-y-0.5 font-medium dark:text-white text-left">
+            @if (old('status', $item->status ? '1' : '0') === "1")
+              <label for="status">Status: 完了</label>
+            @else
+              <label for="status" class="text-red-600">Status: 未着手</label>
+            @endif
+            <a href="{{ route('task_edit', ['id'=>$item->id]) }}" class="text-base m-1 bg-yellow-100 hover:bg-yellow-200 text-blue-900 py-2 px-4 border border-blue-900 rounded-full shadow">編集</a>
+            <a href="{{ route('task_delete', ['id'=>$item->id]) }}" class="text-base m-1 bg-pink-100 hover:bg-pink-200 text-blue-900 py-2 px-4 border border-blue-900 rounded-full shadow">削除</a>
+          <div class="pt-6 flex justify-center">
+            <a href="{{ route('task_index') }}" class="m-1 bg-yellow-100 hover:bg-yellow-200 text-blue-900 py-2 px-10 border border-blue-900 rounded-full shadow">ToDo Listへ</a>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  </table>
 </x-app-layout>
