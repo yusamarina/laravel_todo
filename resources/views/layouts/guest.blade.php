2c42bea5ca1bc7d28f940543317cb6896514b892
yusamarina <94360716+yusamarina@users.noreply.github.com>
2023-02-03 18:39:53 +0900

    Add:トップページの作成

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,16 +14,20 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+        <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div class="shrink-0 flex items-center">
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="{{ asset('img/todo.png') }}" alt="TOP PAGE" class="block h-30 w-auto fill-current">
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mb-12 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
     </body>
+    <footer class="py-4 text-center text-blue-900">
+        Illustration by <a href="https://icons8.com/illustrations/author/JTmm71Rqvb2T">Dani Grapevine</a> from <a href="https://icons8.com/illustrations">Ouch!</a>
+        <p>Copyright © 2022 ToDo App</p>
+    </footer>
 </html>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,6 +3,11 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
+                <div class="shrink-0 flex items-center">
+                    <a href="/">
+                        <img src="{{ asset('img/todo.png') }}" alt="TOP PAGE" class="block h-12 w-auto fill-current text-gray-800">
+                    </a>
+                </div>
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('task_index')" :active="request()->routeIs('task_index')">
