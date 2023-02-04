f3179fcd13ce46b6a20eebdb97ab7239061546ca
yusamarina <94360716+yusamarina@users.noreply.github.com>
2023-02-04 17:20:10 +0900

    Fix:完了したタスクの検索機能を修正

--- resources/views/task/done.blade.php
+++ resources/views/task/done.blade.php
@@ -6,6 +6,15 @@
   </x-slot>
 
   <div class="flex justify-center text-xl pt-6">
+    <form action="{{ route('done_task') }}" method="GET">
+      <div class="flex items-center border-b border-teal-500">
+        <input type="text" name="keyword" value="{{ $keyword }}" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none">
+        <input type="submit" value="検索" class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
+      </div>
+    </form>
+  </div>
+
+  <div class="flex justify-center text-xl pt-6">
     <form action="{{route('done_task')}}">
       <button type="submit" name="sort" value="0" class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
         作成日順
